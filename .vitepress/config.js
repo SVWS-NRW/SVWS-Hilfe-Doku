3914755162db9f7fc5b696971800486599b6b33b
David Klein <david.klein@msb.nrw.de>
2023-01-05 11:20:13 +0100

    new file:   .gitignore 	new file:   .gitlab-ci.yml 	new file:   .vitepress/config.js 	new file:   package-lock.json 	new file:   package.json
    	new file:   .gitignore
    	new file:   .gitlab-ci.yml
    	new file:   .vitepress/config.js
    	deleted:    README.md
    	new file:   index.md
    	new file:   package.json

---
+++ .vitepress/config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  title: 'SVWS Hilfe',
+  description: 'Hilfeseiten für den SVWS-Server NRW',
+  themeConfig: {
+    sidebar: {
+
+
+    }
+  }
+};
+
---
+++ index.md
@@ -0,0 +1,2 @@
+# SVWS-Hilfe-Doku
+
