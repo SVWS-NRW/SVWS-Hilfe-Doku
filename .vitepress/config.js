451b525e5bd415bfc6e7f0e7d91ed1eb5bf1201a
david.klein@msb.nrw.de <root@help.svws-nrw.de>
2023-03-11 15:36:32 +0100

    modified:   .vitepress/config.js 	new file:   kataloge/faecher/index.md 	new file:   kataloge/foerderschwerpunkte/index.md 	new file:   kataloge/jahrgaenge/index.md 	new file:   kataloge/religionen/index.md 	new file:   schule/benutzerverwaltung/index.md 	new file:   schule/datenaustausch/bird/index.md 	new file:   schule/datenaustausch/index.md 	new file:   schule/datenaustausch/lernplattform/index.md 	new file:   schule/datenaustausch/lupo/index.md 	new file:   schule/datenaustausch/msb-fachanwendungsportal/index.md 	new file:   schule/datenaustausch/schueler_online/index.md 	new file:   schule/datenaustausch/wenom/index.md

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,8 +1,45 @@
-﻿module.exports = {
+module.exports = {
+
   title: 'SVWS Hilfe',
-  description: 'Hilfeseiten für den SVWS-Server NRW',
+  description: 'Hilfen SVWS-Server NRW',
   themeConfig: {
+
     sidebar: {
+
+
+
+  '/':
+        [
+          {
+                text: '',
+                items:
+                [
+                  { text: 'Schule', link: '/schule/index.md', collapsed: true,  items:
+			[
+	                  { text: 'Datenaustausch', link: './benutzerverwaltung/index.md' },
+        	          { text: 'Benutzerverwaltung', link: './datenaustausch/index.md' }
+			]
+		  },
+                  { text: 'Kataloge', link: '/kataloge/index.md', collapsed: true,  items:
+
+			 [
+                          { text: 'Förderschwerpunkte', link: './katrderschwerpunkte' },
+                          { text: 'Jahrgänge', link: './katrgaenge' },
+                          { text: 'Religionen', link: './religionen' }
+                         ]
+
+		   },
+                  { text: 'Schüler', link: '/schueler/' },
+                  { text: 'Lehrkräfte', link: '/lehrer/' },
+                  { text: 'Klassen', link: '/klassen/' },
+                  { text: 'Kurse', link: '/kurse/' },
+                  { text: 'Oberstufe', link: '/gost/' },
+                  { text: 'Statistik', link: '/statistik/' }
+
+                ]
+          }
+        ]
+
 
 
     }
---
+++ schule/datenaustausch/index.md
@@ -0,0 +1,12 @@
+# datenaustausch
+
+Sie können auf diversen Wegen Daten mit anderen Diensten austuascaun.hImGrundprinzip müssen die 
+Server in den entspechenden Bereichen eingetragen werden und für das entsprechende Verschlüsselungsverfahren die nötigen Eintragungen vorganommen werden. 
+
+
+[wenom](./wenom/index.md)
+[lernplattform](./lernplattform/index.md)  
+[lupo](./lupo/index.md)  
+[msb-fachanwendungsportal](./msb-fachanwendungsportal/index)  
+[schueler_online](./schueler_online/index.md)  
+[bird](./bird/index.md)
