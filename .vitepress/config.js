e3dcbd2ceacf96c4af64e7e76fb7e3ed1730a13f
David Klein <david.klein@msb.nrw.de>
2023-03-13 22:26:53 +0100

    modified:   .vitepress/config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -25,70 +25,63 @@ sidebar:
 
 				{ text: 'Kataloge', link: '/kataloge/', collapsed: true,  items:
 					[
-						{ text: 'Fächer', link: './facher/' },
-						{ text: 'Förderschwerpunkte', link: './foerderschwerpunkte/' },
-						{ text: 'Jahrgänge', link: './jahrgaenge/' },
-						{ text: 'Religionen', link: './religionen/' }
+						{ text: 'Fächer', link: '/kataloge//facher/' },
+						{ text: 'Förderschwerpunkte', link: '/kataloge/foerderschwerpunkte/' },
+						{ text: 'Jahrgänge', link: '/kataloge/jahrgaenge/' },
+						{ text: 'Religionen', link: '/kataloge/religionen/' }
 					]
 				},
 
 				{ text: 'Schüler', link: '/schueler/', collapsed: true,  items:
 					[
-						{ text: 'Individualdaten', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' }
+						{ text: 'Individualdaten', link: '/schueler/individualdaten/' },
+						{ text: 'Erziehungsberechtigte', link: '/schueler/erziehungsberechtigte/' },
+						{ text: 'Ausbildungsbetriebe', link: '/schueler/ausbildungsbetriebe/' },
+						{ text: 'Schulbesuch', link: '/schueler/schulbesuch/' },
+						{ text: 'Lernabschnitt', link: '/schueler/lernabschnitt/' },
+						{ text: 'Leistungsdaten', link: '/schueler/leistungsdaten/' },
+						{ text: 'Laufbahnplanung', link: '/schueler/laufbahnplanung/' },
+						{ text: 'Stundenplan', link: '/schueler/stundenplan/' }
 					]
 				},
 
 				{ text: 'Lehrkräfte', link: '/lehrer/', collapsed: true,  items:
 					[
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' }
+						{ text: 'Individualdaten', link: '/lehrer/individualdaten/' },
+						{ text: 'Personaldaten', link: '/lehrer/personaldaten/' },
+						{ text: 'Unterricht', link: '/lehrer/unterricht/' },
+						{ text: 'Stundenplan', link: '/lehrer/stundenplan/' }
 					]
 				},
 
 				{ text: 'Klassen', link: '/klassen/', collapsed: true,  items:
 					[
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' }
+						{ text: 'Daten', link: '/klassen/daten/' }
 					]
 				},
 
 				{ text: 'Kurse', link: '/kurse/', collapsed: true,  items:
 					[
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' }
+						{ text: 'Daten', link: '/kurse/daten/' }
 					]
 				},
 
 				{ text: 'Oberstufe', link: '/gost/', collapsed: true,  items:
 					[
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' }
+						{ text: 'Allgemein', link: '/gost/allgemein/' },
+						{ text: 'Fächer', link: '/gost/faecher/' },
+						{ text: 'Fachwahlen', link: '/gost/fachwahlen/' },
+						{ text: 'Kursplanung', link: '/gost/kursplanung/' },
+						{ text: 'Klausurplanung', link: '/gost/klausurplanung/' }
 					]
 				},
 
 				{ text: 'Statistik', link: '/statistik/', collapsed: true,  items:
 					[
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' },
-						{ text: '', link: './/' }
+						{ text: 'Verschlüsselung ', link: '/statistik/verschluesselung' },
+						{ text: 'Datenprüfung', link: '/statistik/datenpruefung' },
+						{ text: 'Dokumente', link: '/statistik/dokumente' },
+						{ text: 'Hilfe', link: '/statistik/hilfe' }
 					]
 
 				}
